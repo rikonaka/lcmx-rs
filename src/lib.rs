df018be48e816d6f7bce8b281caa935af95bb161
Riko <xxy1836@gmail.com>
2024-06-02 17:43:17 +0800

    Only support `usize` type now

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,12 @@
-use gcdx::gcdx;
-use std::fmt::Display;
-use std::ops::Div;
-use std::ops::Mul;
-use std::ops::Rem;
+use gcdx::gcd_euclidean;
 
-fn lcm<T>(a: T, b: T) -> T
-where
-    T: Rem<Output = T>
-        + PartialOrd
-        + Mul<Output = T>
-        + Div<Output = T>
-        + Copy
-        + Display
-        + Clone
-        + gcdx::Zero,
-{
+fn lcm(a: usize, b: usize) -> usize {
     let tmp = a * b;
-    let g = gcdx(&vec![a, b]).unwrap();
+    let g = gcd_euclidean(a, b).unwrap();
     tmp / g
 }
 
-pub fn lcmx<T>(values: &[T]) -> Option<T>
-where
-    T: Rem<Output = T>
-        + PartialOrd
-        + Mul<Output = T>
-        + Div<Output = T>
-        + Copy
-        + Display
-        + Clone
-        + gcdx::Zero,
+pub fn lcmx(values: &[usize]) -> Option<usize>
 {
     if values.len() > 0 {
         let mut m = values[0];
@@ -47,17 +24,17 @@ mod tests {
     use super::*;
     #[test]
     fn run() {
-        let v: Vec<u8> = vec![1, 2, 3, 4];
+        let v: Vec<usize> = vec![1, 2, 3, 4];
         let l = lcmx(&v).unwrap();
         // println!("{}", l);
         assert_eq!(l, 12);
 
-        let v: Vec<u16> = vec![1, 2, 3, 99];
+        let v: Vec<usize> = vec![1, 2, 3, 99];
         let l = lcmx(&v).unwrap();
         // println!("{}", l);
         assert_eq!(l, 198);
 
-        let v: Vec<u32> = vec![1, 2];
+        let v: Vec<usize> = vec![1, 2];
         let l = lcmx(&v).unwrap();
         // println!("{}", l);
         assert_eq!(l, 2);

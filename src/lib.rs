2ebd3cabc408a6f89d5c400fd7a0c2b8fd67abc1
Riko <xxy1836@gmail.com>
2024-06-02 18:29:55 +0800

    Fix readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,35 @@
-use gcdx::gcd_euclidean;
+use gcdx::gcdx;
+use std::fmt::Display;
+use std::ops::Div;
+use std::ops::Mul;
+use std::ops::Rem;
 
-fn lcm(a: usize, b: usize) -> usize {
+fn lcm<T>(a: T, b: T) -> T
+where
+    T: Rem<Output = T>
+        + PartialOrd
+        + Mul<Output = T>
+        + Div<Output = T>
+        + Copy
+        + Display
+        + Clone
+        + gcdx::Zero,
+{
     let tmp = a * b;
-    let g = gcd_euclidean(a, b);
+    let g = gcdx(&vec![a, b]).unwrap();
     tmp / g
 }
 
-pub fn lcmx(values: &[usize]) -> Option<usize>
+pub fn lcmx<T>(values: &[T]) -> Option<T>
+where
+    T: Rem<Output = T>
+        + PartialOrd
+        + Mul<Output = T>
+        + Div<Output = T>
+        + Copy
+        + Display
+        + Clone
+        + gcdx::Zero,
 {
     if values.len() > 0 {
         let mut m = values[0];
@@ -23,7 +46,7 @@ pub fn lcmx(values: &[usize]) -> Option<usize>
 mod tests {
     use super::*;
     #[test]
-    fn run() {
+    fn test_lcmx() {
         let v: Vec<usize> = vec![1, 2, 3, 4];
         let l = lcmx(&v).unwrap();
         // println!("{}", l);
